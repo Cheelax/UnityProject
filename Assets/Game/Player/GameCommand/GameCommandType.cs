7d49606462cfe4d1707f6a4222321ea3f3421ac6
Thomas Belloc <Thomas.Belloc@gmail.com>
2018-08-05 21:33:39 +0200

    Comit initial

---
+++ Assets/Game/Player/GameCommand/GameCommandType.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Gamekit3D.GameCommands
+{
+
+    public enum GameCommandType
+    {
+        None,
+        Activate,
+        Deactivate,
+        Open,
+        Close,
+        Spawn,
+        Destroy,
+        Start,
+        Stop
+    }
+}
